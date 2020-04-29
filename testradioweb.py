9c269d7f26e570c8e38a7d7cddc062c5a90ceab2
LL Productions FR <CrocodileCroco@users.noreply.github.com>
2020-04-29 20:25:47 +0200

    Update testradioweb.py

--- testradioweb.py
+++ testradioweb.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<html><head><title>TestRadio</title><meta charset='UTF-8'></head><body>LiSTEN @ /static/radio.m3u !</body></html>"
+    return "<html><head><title>TestRadio</title><meta charset='UTF-8'></head><body>LiSTEN @ /static/stream.m3u8 !</body></html>"
 
 if __name__ == "__main__":
     app.run(port=8081)
