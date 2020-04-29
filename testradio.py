3a2ee57b4f2510464b66e358c050bb3bb8754b30
LL Productions FR <CrocodileCroco@users.noreply.github.com>
2020-04-29 20:25:09 +0200

    Code that works better

--- testradio.py
+++ testradio.py
@@ -10,18 +10,33 @@ print(os.getcwd())
 
 audiolist = glob.glob(os.getcwd() + "/audiofiles/*.mp3")
 
+streaminit = open("./static/stream.m3u8", "w")
+
+streaminit.write("""#EXTM3U
+#EXT-X-VERSION:3
+#EXT-X-TARGETDURATION:0
+#EXT-X-MEDIA-SEQUENCE:0
+#EXT-X-PLAYLIST-TYPE:EVENT
+""")
+
+streaminit.close()
+
+segmentnumber = 0
+
 while True:
     for i in audiolist:
         print("Now playing " + i + " for " + str(MP3(i).info.length))
         song = AudioSegment.from_mp3(i)
-        #try:
-        #    copyfile(i, os.getcwd() + "/static/radio.mp3")
-        #except:
-        #    Path(os.getcwd() + '/static/radio.mp3').touch()
-        #    copyfile(i, os.getcwd() + "/static/radio.mp3")
         lengthaudio = MP3(i).info.length
-        for i, chunk in enumerate(song[::20000]):
-            with open(os.getcwd() + "/static/radio.mp3", "wb") as f:
-                chunk.export(f, format="mp3")
-                print("exported chunk")
-                time.sleep(3)
+        if not os.path.exists('./static/chunks/' + str(segmentnumber)):
+            os.mkdir('./static/chunks/' + str(segmentnumber))
+        for i, chunk in enumerate(song[::10000]):
+            with open(os.getcwd() + "/static/chunks/" + str(segmentnumber) + "/chunk-%s.mp3" % i, "wb") as f:
+                chunk.export(f, format="mp3", bitrate="120k") #Change the Bitrate if you want
+                print("Chunk Exported")
+                m3ufile = open("./static/stream.m3u8", "a+")
+                m3ufile.write("\n#EXTINF:4.00,\n./chunks/" + str(segmentnumber) + "/chunk-%s.mp3" % i)
+                print("m3u appended")
+                m3ufile.close()
+                time.sleep(2) #Change this to whatever you want
+        segmentnumber = segmentnumber + 1
